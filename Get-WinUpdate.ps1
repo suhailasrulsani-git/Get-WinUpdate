589f69dcf81c5676009f26d7c6a668d72dbd1489
suhailasrulsani-git <124322527+suhailasrulsani-git@users.noreply.github.com>
2023-02-05 05:30:12 +0800

    Update Get-WinUpdate.ps1

--- Get-WinUpdate.ps1
+++ Get-WinUpdate.ps1
@@ -11,31 +11,40 @@ function Get-WinUpdate {
         Invoke-Command -ComputerName $server -EnableNetworkAccess -ScriptBlock {
             $data_raw = Get-WinEvent -LogName "Setup" | Select-Object -Property * | Where-Object { $_.Message -like "*successfully changed to the Installed*" }
             
+            $i = 0
             foreach ($data in $data_raw) {
-                $server_name = $data.MachineName
-    
+
+                if ($i -eq 0) {
+                    $server_name = $data.MachineName
+                }
+                else {
+                    $server_name = ""
+                }
+            
                 $kb_name = $data.Message | `
                     ForEach-Object { $_ -replace ("successfully changed to the Installed.", "") `
                         -replace ("Package ", "") `
                         -replace ("was state.", "") }
-    
+        
                 $Install_date = $data.TimeCreated
-    
+        
                 $user = $data.UserID
                 if ($data.UserID -eq "S-1-5-18") {
                     $user = "System"
                 }
-
+    
                 $last_update = (New-TimeSpan -Start ($data.TimeCreated) -End (Get-Date)).Days
-
+    
                 $status = ""
                 if ($last_update -lt "30") {
                     $status = "Not OK"
                 }
-
+    
                 else {
                     $status = "OK"
                 }
+
+                $total_patch_installed = $data_raw.Length
 
                 [PSCustomObject]@{
                     Server              = $server_name
@@ -44,12 +53,15 @@ function Get-WinUpdate {
                     User                = $user
                     "LastUpdate (Days)" = $last_update
                     Status              = $status
+                    TotalPatch          = $total_patch_installed
                 }
+
+                $i = $i + 1
             }
         }
     }
 
-    $result | Select-Object Server, KB, InstallDate, User, "LastUpdate (Days)", Status | Format-Table -AutoSize
+    $result | Select-Object Server, KB, InstallDate, User, "LastUpdate (Days)", Status, TotalPatch | Format-Table -AutoSize
 }
 
-Get-WinUpdate -object $env:COMPUTERNAME
+Get-WinUpdate -object DESKTOP-9CKGQK2, DESKTOP-9CKGQK2
